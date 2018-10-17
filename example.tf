8f5a9c64dc206e00b24f0324edec1177fb94fc40
Matthew Brown Cisco <mattbro2@cisco.com>
2018-10-17 15:09:07 -0500

    dded shit

---
+++ example.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "tfauto_awesome" {
+  ami           = "ami-06b5810be11add0e2"
+  instance_type = "t2.micro"
+}
+
